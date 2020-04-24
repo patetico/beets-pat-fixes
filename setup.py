8aa9beb9142889530271987935df9f10bdcb4284
patetico <pat@pateti.com>
2020-04-24 18:51:09 -0400

    First commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='beets-pat-fixes',
+    version='0.1.0',
+    description='beets plugin to fix fulfill some ocd requirements',
+    author='Patetico',
+    author_email='pat@pateti.com',
+    url='https://github.com/patetico/beets-pat-fixes',
+    license='MIT',
+    platforms='ALL',
+    packages=['beetsplug'],
+    install_requires=['beets'],
+)
